3170b4a5f76578733a432f705f9a21faf19fc3c5
CH4ER1 <150778909+CH4ER1@users.noreply.github.com>
2025-06-12 14:09:16 +0900

    환경설정 코드

--- SettingActivity.kt
+++ SettingActivity.kt
@@ -1,10 +1,16 @@
 package com.example.finalproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.jvm.java
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,27 @@ class SettingActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // 로그아웃
+        val logoutButton: Button = findViewById(R.id.btnLogout)
+        logoutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            Toast.makeText(this@SettingActivity, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        // 하단 버튼
+        findViewById<ImageView>(R.id.btnWritePost).setOnClickListener {
+            startActivity(Intent(this, ArchiveActivity::class.java))
+        }
+
+        findViewById<ImageView>(R.id.btnAll).setOnClickListener {
+            startActivity(Intent(this, MainScreenActivity::class.java))
+        }
+
+        findViewById<ImageView>(R.id.btnBookmark).setOnClickListener {
+            startActivity(Intent(this, BookmarkActivity::class.java))
         }
     }
 }
