d83e12b035e4faf3b62860d60baf30fc94d86382
CH4ER1 <150778909+CH4ER1@users.noreply.github.com>
2025-06-12 14:14:31 +0900

    즐겨찾기 저장 코드

---
+++ BookmarkData.kt
@@ -0,0 +1,8 @@
+package com.example.finalproject
+
+data class BookmarkData(
+    val postId: String = "",
+    val postTitle: String="",
+    val postAuthor: String="",
+    val bookmarkedAt: Long=0L
+)
